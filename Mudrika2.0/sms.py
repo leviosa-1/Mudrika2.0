6056548f9fd7c627d52aed56e5a51f18abe6dbed
leviosa-1 <119951206+leviosa-1@users.noreply.github.com>
2024-04-06 15:38:32 +0530

    Update sms.py

--- Mudrika2.0/sms.py
+++ Mudrika2.0/sms.py
@@ -1,10 +1,10 @@
 from twilio.rest import Client
 def send_sms():
-    account_sid = "AC091ddcdbd3f3a86db5c7ab71145435fe"
-    auth_token = "6c9750c149dbe1580668098e140f319a"
+    account_sid = "Twillio account_sid"
+    auth_token = "Twillio auth_token"
     client = Client(account_sid, auth_token)
-    to_number = "+917024896018"
-    from_number = "+16592518494"
+    to_number = "receiver's number"
+    from_number = "Twillio phone number"
     message_body = "ALert!!! someone needs your help at Location:- 22.7081955, 75.8824422"
     message = client.messages.create(
     body=message_body,
